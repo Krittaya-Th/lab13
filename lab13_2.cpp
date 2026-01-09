2a6a885fecfd682e237ac4b6b945c52df5bd9ada
Krittaya-Th <krittayaclf9028@gmail.com>
2026-01-09 21:21:16 +0700

    lab13_2_completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,34 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool I[][M]){
+    for(int i=0;i<32;i++){
+        for(int j=0;j<72;j++){
+            if(i==0 || i==31){
+                cout << "-";
+            }else if (j==0 || j==71){
+                    cout << "|";
+            }else if (I[i-1][j-1]){
+                cout << "*";
+                
+            }else{
+                cout << " ";
+            }
+        }
+        cout << "\n";
+    }
+}
+
+void updateImage(bool A[][M],int n,int p,int q){
+     for(int i=0;i<N+2;i++){
+        for(int j=0;j<M+2;j++){
+            if((sqrt((pow(i-p,2)+pow(j-q,2))))<=n-1){
+                if(i<N&&j<M){
+                    A[i][j]=true; 
+                }
+                   
+            }
+        }
+    }
+}
