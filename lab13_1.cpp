f365b02eb19f2b74a1e4249fd4c0156a0ee46994
Krittaya-Th <krittayaclf9028@gmail.com>
2026-01-09 18:15:25 +0700

    lab13_1_completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,24 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[]){
+    double sum=0,mean,harmean=0,geo=1,geomean,std=0,stdv,max=A[0],min=A[0];
+    for(int i=0;i<N;i++){
+        
+        sum += A[i];
+        mean = sum/N;
+        harmean += 1/(A[i]);
+        geo *= A[i];
+        geomean = pow(geo,pow(N,-1));
+        if(A[i]>max) max = A[i];
+        if(A[i]<min) min = A[i];
+        std += pow(A[i],2);
+        stdv = sqrt((std/N)-pow(mean,2));
+    }
+    B[0] = mean;
+    B[1] = stdv;
+    B[2] = geomean;
+    B[3] = N/harmean;
+    B[4] = max;
+    B[5] = min;
+}
